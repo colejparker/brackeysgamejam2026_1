0bcbe2d6484cb01ff9136a8cd635adf3199df3d1
Keegan Parker <keegandparker@gmail.com>
2026-02-16 15:58:50 -0700

    Inventory wip

--- brackeysgamejam-2026-1/inventory_slot.gd
+++ brackeysgamejam-2026-1/inventory_slot.gd
@@ -1,12 +1,18 @@
 extends PanelContainer
 
 @export var furniture_data: FurnitureData
+@export var is_active: bool = false
 @onready var sprite = $Sprite2D
+@onready var active_slot = $ActiveSlot
 
 func _ready() -> void:
-	if furniture_data:
+	update_for_change()
+
+func update_for_change() -> void:
+	if furniture_data != null:
 		sprite.texture = furniture_data.texture
 		sprite.hframes = 2 if furniture_data.is_wall else 4
 		sprite.frame = 0
 	else:
 		sprite.texture = null
+	active_slot.visible = is_active
--- brackeysgamejam-2026-1/src/global/game.gd
+++ brackeysgamejam-2026-1/src/global/game.gd
@@ -3,13 +3,23 @@ extends Node
 @export var next_bugger_level: PackedScene = preload("res://src/bugger/levels/bugger_1.tscn")
 @export var next_furniture_item: PackedScene
 @export var cost_of_next_item: float = 1.0
+var inventory: Array[FurnitureData] = []
+var selected_inventory_slot = 0
 
 var pause_menu: PackedScene = preload("res://src/global/ui/pause_menu.tscn")
 
 signal money_changed(new_amount: int)
+signal add_item_to_inventory(name: String)
+signal remove_item_from_inventory(name: String)
+signal inventory_updated()
+
+var furniture_catalog: Dictionary[String, FurnitureData] = {}
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	prepare_furniture_catalog()
+	add_item_to_inventory.connect(_on_add_item_to_inventory)
+	remove_item_from_inventory.connect(_on_remove_item_from_inventory)
 
 var money: int = 5:
 	set(value):
@@ -24,6 +34,13 @@ func die_lose_money():
 	
 func load_next_bugger_level():
 	get_tree().change_scene_to_packed(next_bugger_level)
+	
+func prepare_furniture_catalog():
+	const FURNITURE_RESOURCE_DIR = "res://src/global/Furniture(Resources)/"
+	for file_name in DirAccess.get_files_at(FURNITURE_RESOURCE_DIR):
+		if file_name.ends_with(".tres"):
+			var furniture_data = ResourceLoader.load(FURNITURE_RESOURCE_DIR + file_name) as FurnitureData
+			furniture_catalog.set(furniture_data.name, furniture_data)
 
 func _input(event):
 	if event.is_action_pressed("escape"):
@@ -38,3 +55,18 @@ func _input(event):
 			var pause_node = get_tree().current_scene.get_node_or_null("PauseMenu")
 			if pause_node:
 				pause_node.queue_free()
+				
+func _on_add_item_to_inventory(name: String):
+	inventory.append(furniture_catalog.get(name))
+	inventory_updated.emit()
+	print(inventory)
+	
+func _on_remove_item_from_inventory(name: String):
+	var index_to_remove = -1
+	for i in inventory.size():
+		if inventory[i].name == name:
+			index_to_remove = i
+			break
+	if index_to_remove >= 0:
+		inventory.remove_at(index_to_remove)
+	inventory_updated.emit()
