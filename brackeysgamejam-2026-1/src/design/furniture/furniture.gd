9a11d7c122cea37492e35ddcd315ec05e17c7928
Keegan Parker <keegandparker@gmail.com>
2026-02-15 21:14:45 -0700

    Wall Furniture support

--- brackeysgamejam-2026-1/src/design/furniture/furniture.gd
+++ brackeysgamejam-2026-1/src/design/furniture/furniture.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var parent_tile_map_layer = get_parent() as TileMapLayer
 @onready var collection_shape = $CollisionShape2D
-@onready var sprite =$Sprite2D
+@onready var sprite = $Sprite2D
 @export var is_wall = false
 
 enum State {
@@ -16,25 +16,34 @@ func _physics_process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var mouse_pos = get_local_mouse_position()
 		if state == State.STATIONARY and mouse_in_rect():
-			print("!")
 			state = State.MOVING
 			
 	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		state = State.STATIONARY
 		
-	if Input.is_action_just_pressed("rotate") and (mouse_in_rect() or state == State.MOVING):
+	if Input.is_action_just_pressed("rotate") and (mouse_in_rect() or state == State.MOVING) and !is_wall:
 		sprite.frame = (sprite.frame + 1) % sprite.hframes
+	
+	if is_wall:
+		sprite.frame = 0 if position.x <= 350 else 1
 	
 	if state == State.MOVING:
 		var desired_position = get_desired_position(Input.is_action_pressed("snapToGrid"))
 		var motion = (desired_position - position)
 		move_and_collide(motion)
 
-
 func mouse_in_rect() -> bool:
 	return collection_shape.get_shape().get_rect().has_point(get_local_mouse_position())
 	
 func get_desired_position(snap: bool) -> Vector2:
+	var desired_pos = get_global_mouse_position()
 	if snap:
-		return parent_tile_map_layer.map_to_local(parent_tile_map_layer.local_to_map(get_global_mouse_position()))
-	return get_global_mouse_position()
+		return parent_tile_map_layer.map_to_local(parent_tile_map_layer.local_to_map(desired_pos))
+	if desired_pos.x >= 338 and desired_pos.x <= 362:
+		desired_pos.x = 338
+	return desired_pos
+
+func _ready():
+	if is_wall:
+		sprite.texture = preload("res://poster.png")
+		sprite.hframes = 2
