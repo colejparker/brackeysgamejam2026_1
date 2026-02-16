f90330afa5f7d009c7e92779176f97274ab7a364
Cole Parker <cole.parker@hotmail.ca>
2026-02-15 20:50:55 -0700

    Add Placeholder Main Menu and Theming

--- brackeysgamejam-2026-1/src/bugger/ui/money_tracker.gd
+++ brackeysgamejam-2026-1/src/bugger/ui/money_tracker.gd
@@ -1,9 +1,5 @@
 extends Label
 
-@export var next_bugger_level: PackedScene
-@export var next_furniture_item: PackedScene
-@export var cost_of_next_item: float = 1.0
-
 func _ready():
 	Game.money_changed.connect(_on_money_changed)
 	_update_display(Game.money)
--- brackeysgamejam-2026-1/src/global/game.gd
+++ brackeysgamejam-2026-1/src/global/game.gd
@@ -1,6 +1,13 @@
 extends Node
 
+@export var next_bugger_level: PackedScene
+@export var next_furniture_item: PackedScene
+@export var cost_of_next_item: float = 1.0
+
 signal money_changed(new_amount: int)
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 var money: int = 5:
 	set(value):
@@ -12,3 +19,15 @@ func add_money(amount: int):
 	
 func die_lose_money():
 	money -= floor(money/2)
+	
+func _input(event):
+	if event.is_action_pressed("escape"):
+		if get_tree().current_scene.scene_file_path.contains("game.tscn"):
+			return
+		get_tree().paused = !get_tree().paused
+		# To-Do: Create Pause Menu to Instantiate Here
+		if get_tree().paused:
+			print("Game Paused")
+		else:
+			print("Game Resumed")
+		
